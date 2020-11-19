5b04a0417d535f0fe358d7fbf8cf9068c33991cf
leafiy <t@leafiy.com>
2020-11-20 05:18:44 +0800

    upload

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = function chunk(array, size) {
+  if (!array) return [];
+  const firstChunk = array.slice(0, size);
+  if (!firstChunk.length) {
+    return array;
+  }
+  return [firstChunk].concat(chunk(array.slice(size, array.length), size));
+};
